283b8908a8dcc9670193ecc262cb261150e83e9d
Wildan Zulfikar <hey@wzulfikar.com>
2022-09-12 13:43:48 +0300

    Load job files from the same directory as jobs/index.js

--- jobs/index.js
+++ jobs/index.js
@@ -1,14 +1,16 @@
 const fs = require("fs");
+const path = require("path");
 const faktory = require("faktory-worker");
 
-const jobsDir = process.argv[process.argv.indexOf("-r") + 1];
+const jobsDir = __dirname;
 
 fs.readdir(jobsDir, (err, files) => {
   files.forEach((file) => {
     if (err) throw new Error(err);
     if (file == "index.js") return;
 
-    const job = require(`${jobsDir}/${file}`);
+    // Register job files in current directory
+    const job = require(path.join(jobsDir, file));
     faktory.register(job.name, job);
 
     console.log("- Job registered:", job.name);
